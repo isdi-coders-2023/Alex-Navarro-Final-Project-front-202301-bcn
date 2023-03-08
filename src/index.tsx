adbc5a7e9c51ee1ebab7e5f788284d7ab7450d01
Alex Navarro <alexnavter@gmail.com>
2023-03-08 15:39:11 +0100

    Create GlobalStyles, install style-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
+import App from "./components/App/App";
 import { store } from "./store";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Provider store={store}>
       <App />
     </Provider>
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+
+
+*, ::after, ::before {
+  box-sizing: border-box;
+}
+
+
+body {
+  margin: 0;
+  font-family: Helvetica, sans-serif;
+  font-size: 1rem;
+  background-color: #000;
+  min-height: 100vh;
+}
+
+  
+h1,h2{
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+ol, ul, li {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+
+button, input {
+  font-family: inherit;
+  font-size:inherit;
+  border: none;
+}
+  
+button {
+  background-color: transparent;
+  cursor: pointer;
+}
+
+a, a:active, a:visited {
+    color: inherit;
+    text-decoration: none;
+  }
+
+
+`;
+
+export default GlobalStyles;
