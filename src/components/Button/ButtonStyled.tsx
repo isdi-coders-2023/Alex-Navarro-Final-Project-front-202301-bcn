67c45f20e12b840a99231c902c72d936634262ef
Alex Navarro <alexnavter@gmail.com>
2023-03-08 21:32:12 +0100

    Add styles to Button component

---
+++ src/components/Button/ButtonStyled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  height: 40px;
+  border-radius: 10px;
+  border: solid 2px red;
+  padding: 0.8rem;
+  width: 100%;
+  color: black;
+`;
+
+export default ButtonStyled;
