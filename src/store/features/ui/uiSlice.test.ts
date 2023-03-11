6607eb5529dbfe0867b94c55212bf709ea6b6de1
Alex Navarro <alexnavter@gmail.com>
2023-03-11 18:31:45 +0100

    Test setIsLoading & unSetIsLoading action creators in uiSlice

--- src/store/features/ui/uiSlice.test.ts
+++ src/store/features/ui/uiSlice.test.ts
@@ -1,5 +1,11 @@
-import { ModalPayload } from "./types";
-import { initialState, displayModalActionCreator, uiReducer } from "./uiSlice";
+import { ModalPayload, UiState } from "./types";
+import {
+  initialState,
+  displayModalActionCreator,
+  uiReducer,
+  setIsLoadingActionCreator,
+  unSetIsLoadingActionCreator,
+} from "./uiSlice";
 
 describe("Given a uiReducer", () => {
   describe("When its displayModal action creator is invoked, with the text 'Wrong credentials' and its property isError true", () => {
@@ -18,6 +24,43 @@ describe("Given a uiReducer", () => {
       const newModalState = uiReducer(initialState, displayModal);
 
       expect(newModalState).toStrictEqual(expectedUiState);
+    });
+
+    describe("When its setIsLoading action creator is invoked", () => {
+      test("Then it should set isLoading property to true", () => {
+        const expectedUiState: UiState = {
+          isError: false,
+          isLoading: true,
+          modal: "",
+        };
+
+        const newUiState = uiReducer(initialState, setIsLoadingActionCreator());
+
+        expect(newUiState).toStrictEqual(expectedUiState);
+      });
+    });
+  });
+
+  describe("When its unSetIsLoading action creator is invoked", () => {
+    test("Then it should set isLoading property to false", () => {
+      const expectedUiState: UiState = {
+        isError: false,
+        isLoading: false,
+        modal: "",
+      };
+
+      const currentUiState: UiState = {
+        isError: false,
+        isLoading: true,
+        modal: "",
+      };
+
+      const newUiState = uiReducer(
+        currentUiState,
+        unSetIsLoadingActionCreator()
+      );
+
+      expect(newUiState).toStrictEqual(expectedUiState);
     });
   });
 });
