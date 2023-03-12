e9f5b7a0b47deb777b4aa55a356f673d37bb7c67
Alex Navarro <alexnavter@gmail.com>
2023-03-12 11:24:06 +0100

    Create routes & ProtectedRoute component

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+import endpoints from "../../routes/types";
+import { useAppSelector } from "../../store/hooks";
+
+interface ProtectedRouteProps {
+  element: JSX.Element;
+}
+
+const ProtectedRoute = ({ element }: ProtectedRouteProps): JSX.Element => {
+  const { token } = useAppSelector((state) => state.user);
+
+  return token ? element : <Navigate to={endpoints.login} replace={true} />;
+};
+
+export default ProtectedRoute;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import App from "../components/App/App";
+import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
+import LoginPage from "../Pages/LoginPage";
+import endpoints from "./types";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <ProtectedRoute element={<App />} /> },
+      { path: endpoints.login, element: <LoginPage /> },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
